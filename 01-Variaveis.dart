1d4babe5631bfa1adbda08849919ee726fb5e8c9
Ednelson Amaral <eadamaral@gmail.com>
2021-02-26 17:29:00 -0300

    Create 01-Variaveis.dart
    variaveis

---
+++ 01-Variaveis.dart
@@ -0,0 +1,30 @@
+main() {
+  print('Olá mundo');
+  // Boas práticas ao escrever códigos
+  // Ex: var calcularArea
+
+  var valorA = 10;
+  var valorB = 4;
+  var valorC = 2.5;
+  var resultado = valorA + valorB + valorC;
+  print(resultado);
+
+  var salario = 1000, desconto = 250;
+  var conta = salario - desconto;
+  print(conta);
+
+  var numero = 3 + ((2 * 4) + (10 / 5));
+  print(numero);
+
+  // String
+
+  print('concatenar '
+      'Strings'
+      ' é simples assim!');
+
+  var texto1 = '\n Ednelson ';
+  var texto2 = 'Amaral';
+  var texto3 = '''\n e Bryan Guilherme''';
+  var nome = texto1 + texto2 + texto3;
+  print(nome);
+}
