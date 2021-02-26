734a1cfb0d8e49045b44af51cbff3ebb53089081
Ednelson Amaral <eadamaral@gmail.com>
2021-02-26 18:36:29 -0300

    Variáveis e Funções

--- 01-Variaveis.dart
+++ 01-Variaveis.dart
@@ -1,7 +1,18 @@
+// Variáveis
 main() {
   print('Olá mundo');
   // Boas práticas ao escrever códigos
   // Ex: var calcularArea
+  // Bloco de comentário /* texto */
+  // Comentando linha //
+
+  /* 
+  "backspace" : "\b"
+  "formfeed" : "\f"
+  "newline" : "\n"
+  "retunr" : "\r"
+  "tab" : "\t"
+  */
 
   var valorA = 10;
   var valorB = 4;
@@ -27,4 +38,30 @@ main() {
   var texto3 = '''\n e Bryan Guilherme''';
   var nome = texto1 + texto2 + texto3;
   print(nome);
+
+  var verdadeiro = true;
+  var falso = false;
+  // $ variáveis, ${} operações
+  print('verdadeiro = ${verdadeiro}'); // Interpolação
+  print('falso = ' + "${falso}"); // concatenação
+  print(''.runtimeType); // descobre o tipo de variável em tempo de execução.
+
+  const pi = 3.14; // Variável const é atribuída em tempo de compilação
+  nome = 'Ednelson';
+  String sobrenome = 'Amaral';
+  int idade = 30;
+  double altura = 1.88;
+  bool adulto = true;
+  print('nome: $nome ' +
+      'Qnt. letras: ${nome.length} ' +
+      'Sobre nome: $sobrenome');
+  print('altura: $altura '
+          'adulto: $adulto ' +
+      'idade: $idade');
+
+  dynamic variavel = 2.0;
+  variavel = 2;
+  variavel = 'dois';
+  variavel = true;
+  print(variavel);
 }
---
+++ 02-VariaveisFuncoes.dart
@@ -0,0 +1,10 @@
+// Variáveis e suas funções
+// num{int, double}, string, bool, dynamic
+main() {
+  num pi = 3.14;
+  print(pi.floor); // Aredonda variaveis fracionárias
+  print(pi.round); // Aredonda para o inteiro mais próximo
+  print(pi.ceil); // Arredonta para o inteiro mais próximo maior
+
+  print('');
+}
